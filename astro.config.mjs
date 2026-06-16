457fd9d752d15a21a78e3d26985bc56c68f35fc6
Sayyed Aaman <sayyedaaman9@gmail.com>
2026-06-16 19:06:45 +0530

    added Event for anlyatics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://freeinjurycalc.com',
+  trailingSlash: 'always',
   vite: {
     plugins: [tailwindcss()]
   }
